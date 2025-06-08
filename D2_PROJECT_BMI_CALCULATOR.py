2c0404bee1320873812ae3f8b1dacb2f85471492
Vishal kadam <167006989+Vishalkadam2003@users.noreply.github.com>
2025-06-08 13:41:26 +0530

    Add files via upload

---
+++ D2_PROJECT_BMI_CALCULATOR.py
@@ -0,0 +1,6 @@
+print("Welcome to BMI Calculator")
+weight = int(input("Enter Your Weight : "))
+height = float(input("Enter Your Height(in feets) : "))
+BMI = weight / height**2
+print("Your Body Mass Index is : ",BMI)
+print("Thank You !")
